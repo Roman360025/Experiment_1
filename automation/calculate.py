ada07d6e36704926cd178119f101091f6c4f7b98
Roman360025 <professor360025@gmail.com>
2022-03-19 12:49:54 +0300

    Update calculate.py

--- automation/calculate.py
+++ automation/calculate.py
@@ -1,15 +1,21 @@
 import pandas as pd
+import shutil
+from zipfile import ZipFile
+import os
+
 
 df = pd.DataFrame()
 
-f = open("7.txt")
+sf = 12
+
+f = open(f"{sf}.txt")
 SNR_sum = 0
 count_of_SNR = 0
 power = -12124141
 for line in f:
     if 'Power' in line:
         if count_of_SNR != 0:
-            print(SNR_sum / count_of_SNR, count_of_SNR / 100)
+            # print(SNR_sum / count_of_SNR, count_of_SNR / 100)
             df = df.append({'SNR': SNR_sum / count_of_SNR, 'probability': count_of_SNR / 100}, ignore_index=True)
             SNR_sum = 0
             count_of_SNR = 0
@@ -22,11 +28,10 @@ for line in f:
         count_of_SNR += 1
 
 if count_of_SNR != 0:
-    print(SNR_sum / count_of_SNR, ':', count_of_SNR / 100)
+    df = df.append({'SNR': SNR_sum / count_of_SNR, 'probability': count_of_SNR / 100}, ignore_index=True)
     SNR_sum = 0
     count_of_SNR = 0
 
-
-df = df.sort_values(by='probability')
-df.to_excel('7.xlsx', index=False)
-print(df)
+df = df.sort_values(by=["probability", 'SNR'])
+# df.to_csv('12.csv', mode='a', sep=';', index=False, float_format="%.3f")
+df.to_excel(f'{sf}.xlsx', index=False)
