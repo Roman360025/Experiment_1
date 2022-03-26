f68b264dd14baf6d1e0b12cc449f15ddce1f9e44
Roman360025 <professor360025@gmail.com>
2022-03-26 17:02:56 +0300

    Добавлен вывод сообщений в консоль, имена файлов для данных формируются автоматический

--- automation/listener.py
+++ automation/listener.py
@@ -1,12 +1,12 @@
 import serial
-import time
+import datetime
 
 ser_receiver = None
 
 while not ser_receiver:
     try:
         ser_receiver = serial.Serial(
-            port='COM6',
+            port='COM13',
             baudrate=115200,
             parity=serial.PARITY_NONE,
             stopbits=serial.STOPBITS_ONE,
@@ -21,13 +21,23 @@ power = -1
 sf = 7
 reset = 0
 
+line = ''
 
-with open('{}.txt'.format(sf), 'w') as f:
+filename = 'Experiment_data/Experiment_10_packets'
+
+now = datetime.datetime.now().strftime('%Y-%m-%d')
+
+with open(f'{filename}_{now}.txt', 'a') as f:
     while 1:
         try:
             line_recv = ser_receiver.read(10000000000000000000000000000000000000)
             if line_recv != b'':
-                f.write(line_recv.decode('utf-8'))
+                received_line = line_recv.decode('utf-8')
+                line = line + received_line
+                f.write(received_line)
+            if '\n' in line:
+                print(line)
+                line = ''
         except:
             break
 
